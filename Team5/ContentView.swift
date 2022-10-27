d8ef97a0f0791b702e6e2fd9616306cbb84b6e3e
CSFatimaAJ <csfatimaaljaber@icloud.com>
2022-10-27 11:25:41 +0300

    Initial Commit

---
+++ Team5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Team5
+//
+//  Created by Fatima Aljaber on 27/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team5/Team5App.swift
@@ -0,0 +1,17 @@
+//
+//  Team5App.swift
+//  Team5
+//
+//  Created by Fatima Aljaber on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Team5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
